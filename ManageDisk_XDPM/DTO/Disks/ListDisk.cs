5b817575c5d703cc26f0ed2f9e10f8575ff4bd88
duylinh289 <linhnguyen.dlhn@gmail.com>
2021-08-11 20:54:27 +0700

    uc4 thêm xóa đĩa

---
+++ ManageDisk_XDPM/DTO/Disks/ListDisk.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.Disks
+{
+    public class ListDisk
+    {
+        public int DiskID { get; set; }
+        public string TieuDe { get; set; }
+        public string TheLoai { get; set; }
+        public string TrangThai { get; set; }
+    }
+}
